f026d8396cd35b5bf1e0e2688af678997e7db0e5
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-06-22 20:28:23 -0700

    working cron scheduler

--- cron.py
+++ cron.py
@@ -3,30 +3,42 @@ from schedule import every, repeat, run_pending
 import time
 import requests
 import datetime
+# import twi
 
-data = [{'id': 15, 'title': 'change 15', 'description': 'plase help', 'completed': False, 'due': '1220-05-21T00:00:00Z', 'created_at': '2022-06-22T21:35:51.782434Z', 'updated_at': '2022-06-23T00:29:00.601553Z', 'owner': 1}, {'id': 12, 'title': 'test', 'description': '', 'completed': True, 'due': '2220-05-21T00:00:00Z', 'created_at': '2022-06-22T18:24:49.828285Z', 'updated_at': '2022-06-23T00:29:09.470164Z', 'owner': 1}]
-
-# @repeat(every(1).seconds)
-def job(data):
-    print("this is job")
-    # response = requests.get("https://do-or-doom-api.herokuapp.com/api/v1/tasks/")
-    # data = response.json()
+set_sleep = 1
+@repeat(every(1).seconds)
+def job():
+    global set_sleep
+    response = requests.get("https://do-or-doom-api.herokuapp.com/api/v1/tasks/")
+    data = response.json()
+    print("Hitting tasks api")
     current_datetime = datetime.datetime.now()
-    print("current time--->",current_datetime)
     for x in range(len(data)):
+
         if data[x].get("completed")is False:
+
+            print("found a task that was not completed")
+
             datetime_object = datetime.datetime.strptime(data[x].get("due"), "%Y-%m-%dT%H:%M:%SZ" )
-            # print("current_datetime --->", current_datetime)
-            # print("datetime_object --->", datetime_object)
+
             if current_datetime > datetime_object:
-                print("woopsiedoodle")
-job(data)
+
+                print("task past due! oh NO!!!!!!!!")
+                time.sleep(1)
+                print("DOOM!")
+                set_sleep = 50
+                print("sleeping for: ", set_sleep)
+                # do Twillo stuff here
+                break
+            else:
+                print("You've still got time!")
+# job(data)
 
 
 
-# while True:
-#     run_pending()
-#     time.sleep(1)
-
+while True:
+    run_pending()
+    time.sleep(set_sleep)
+    print("happening")
 
 
